d04721a953ea6ad1736f93b216150dae28eaced0
Cin316 <joe13reich@gmail.com>
2013-03-20 17:19:51 -0400

    Readded Execute.java.

---
+++ src/com/nchat/client/Execute.java
@@ -0,0 +1,16 @@
+package com.nchat.client;
+
+import javax.swing.JOptionPane;
+
+public class Execute {
+
+	public static final int port = 13131;
+	public static String ip;
+
+	public static void main(String[] args) {
+
+		ip = JOptionPane.showInputDialog("Enter the IP address of the server you would like to connect to.");
+
+	}
+
+}

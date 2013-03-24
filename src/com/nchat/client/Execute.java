c8cbf228a7d5903d896bded0539ce104c0b9ba3f
Cin316 <joe13reich@gmail.com>
2013-03-24 09:08:04 -0400

    Added ability to change port with command line argument.

--- src/com/nchat/client/Execute.java
+++ src/com/nchat/client/Execute.java
@@ -4,11 +4,15 @@ import javax.swing.JOptionPane;
 
 public class Execute {
 
-	public static final int port = 13132;
+	public static int port = 13132;
 	public static String ip;
 
 	public static void main(String[] args) {
-
+		
+		if (args.length>=1){
+			port = Integer.parseInt(args[0]);
+		}
+		
 		ip = JOptionPane.showInputDialog("Enter the IP address of the server you would like to connect to.");
 		NChatClient client = new NChatClient(ip);
 	}
